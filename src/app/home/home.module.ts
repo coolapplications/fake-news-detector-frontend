44b34ab3e156cd95b426385ee526586ff4070d04
Andrés Alvarez <amauricio.alvarez@udea.edu.co>
2020-12-29 19:04:04 -0500

    form creation

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,12 +4,22 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
-  ]
+    HomeRoutingModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
+  ],
 })
-export class HomeModule { }
+export class HomeModule {}
